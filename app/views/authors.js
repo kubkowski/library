77e9f34e2834df20bfbca1fe5406264b16642fc7
kubkowski <jakub.opara@gmail.com>
2014-11-05 23:12:48 +0100

    Fixed author list view

--- app/views/authors.js
+++ app/views/authors.js
@@ -1,21 +1,4 @@
-window.AuthorsItemView = Backbone.View.extend({
-	tagName: "tr",
-	template: _.template($("#authors-list-item").html()),
-	render: function() {
-      this.$el.html(this.template(this.model.toJSON()));
-      return this;
-     }
-});
 
-window.AuthorsListView = Backbone.View.extend({
-	el: '.page',
-	render: function(){
-		var self = this;
-    _.each(self.authors.models, function(author) {
-      self.$el.append(new AuthorsItemView({model: author}).render().el);
-    });
-	}
-});
 
 		window.EditAuthorView = Backbone.View.extend({
 			el: '.container',
