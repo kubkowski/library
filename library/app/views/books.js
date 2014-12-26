2ffc363f7b956c5278bc012b67c41efab1db7638
kubkowski <jakub.opara@gmail.com>
2014-12-26 11:20:45 +0100

    Fixed books and publishers views

--- library/app/views/books.js
+++ library/app/views/books.js
@@ -1,37 +1,47 @@
 BooksListHeader = Backbone.View.extend({
+
   el: '.container',
+
   template: _.template($("#books-list").html()),
+
   render: function () {
     this.$el.html(this.template);
   }
+
 });
 
 BooksList = Backbone.View.extend({
+
   el: '.page',
+
   render: function () {
     var self = this;
     var books = new Books();
     var allSubViews = document.createDocumentFragment();
     books.fetch({
-      success: function (books) {
-        _.each(books.models, function (book) {
+      success: function ( books ) {
+        _.each( books.models, function ( book ) {
           allSubViews.appendChild(new BooksItemView({model: book}).render().el);
         });
         self.$el.append(allSubViews);
       }
     });
   }
+
 });
 
 BooksItemView = Backbone.View.extend({
+
   tagName: 'tr',
+
   template: _.template($("#books-list-item").html()),
+
   render: function () {
     var self = this;
     self.book = this.model;
     self.author = new Author({id: this.model.attributes.author_id});
     self.publisher = new Publisher({id: this.model.attributes.publisher_id});
-    $.when( self.author.fetch() , self.publisher.fetch() ).done(function () {
+    $.when( self.author.fetch() , self.publisher.fetch() ).done( function () {
       self.$el.html(self.template({
         author: self.author.attributes,
         book: self.book.attributes,
@@ -40,25 +50,29 @@ BooksItemView = Backbone.View.extend({
     });
     return this;
   }
+
 });
 
 
 EditBook = Backbone.View.extend({
+
   el: '.container',
+
   template: _.template($('#edit-book').html()),
-  render: function (options) {
+
+  render: function ( options ) {
     var self = this;
     self.authors = new Authors();
     self.publishers = new Publishers();
-    $.when( self.authors.fetch() , self.publishers.fetch() ).done(function () {  
-      if (options.id) {
+    $.when( self.authors.fetch() , self.publishers.fetch() ).done( function () {  
+      if ( options.id ) {
       self.book = new Book({id: options.id});
       self.book.fetch({
-        success: function (book) {
+        success: function ( book ) {
           self.book = book;
           self.bookAuthor = new Author({id: self.book.attributes.author_id});
           self.bookPublisher = new Publisher({id: self.book.attributes.publisher_id});
-          $.when( self.bookAuthor.fetch() , self.bookPublisher.fetch() ).done(function() {
+          $.when( self.bookAuthor.fetch() , self.bookPublisher.fetch() ).done( function () {
             self.$el.html(self.template({
               book: self.book.attributes,
               authors: self.authors.models,
@@ -66,7 +80,7 @@ EditBook = Backbone.View.extend({
               author: self.bookAuthor.attributes,
               publisher: self.bookPublisher.attributes
             }));
-          });         
+          });
         }
       });
       } else {
@@ -77,14 +91,16 @@ EditBook = Backbone.View.extend({
         }));
       }
     });
-    
+
   },
+
   events: {
     'submit .edit-book-form': 'saveBook',
     'click .delete' : 'deleteBook',
     'click .return' : 'returnToList'
   },
-  saveBook: function (ev) {
+
+  saveBook: function ( ev ) {
     var self = this;
     self.bookDetails = $(ev.currentTarget).serializeObject();
     self.book = new Book({id: self.bookDetails.id});
@@ -93,11 +109,11 @@ EditBook = Backbone.View.extend({
     });
     self.bookDetails.ISBN = self.bookDetails.ISBN.replace('-','');
     var errors = self.book.preValidate(self.bookDetails);
-    if (errors){
+    if ( errors ) {
       var $errors = $('ul#errors');
       var msgs = _.values(errors);
       $errors.empty();
-      _.each(msgs, function (msg) {
+      _.each( msgs, function ( msg ) {
         $errors.append("<li>"+ msg + "</li>");
       });
     } else {
@@ -111,9 +127,10 @@ EditBook = Backbone.View.extend({
     }
     return false;
   },
+
   deleteBook: function () {
     var self = this;
-    if (confirm('Are you sure you want to delete that book?')) {
+    if ( confirm('Are you sure you want to delete that book?') ) {
       this.book.destroy({
         success: function () {
           self.undelegateEvents();
@@ -123,9 +140,11 @@ EditBook = Backbone.View.extend({
     }
     return false;
   },
+
   returnToList: function () {
     this.undelegateEvents();
     router.navigate('/books', {trigger: true});
     return false;
   }
+
 });
--- library/app/views/publishers.js
+++ library/app/views/publishers.js
@@ -12,28 +12,31 @@ PublishersListHeader = Backbone.View.extend({
 
 PublishersList = Backbone.View.extend({
 
-	el: '.page',
-	render: function(){
-		var self = this;
-		var publishers = new Publishers();
-		var allSubViews = document.createDocumentFragment();
-		publishers.fetch({
-			success: function(publishers){
-					_.each(publishers.models, function(publisher){
-						allSubViews.appendChild(new PublishersItemView({model: publisher}).render().el);
-					});
-					self.$el.append(allSubViews);
-			}
-		});				
-	}
+  el: '.page',
+
+  render: function () {
+    var self = this;
+    var publishers = new Publishers();
+    var allSubViews = document.createDocumentFragment();
+    publishers.fetch({
+      success: function ( publishers ) {
+        _.each( publishers.models, function ( publisher ) {
+          allSubViews.appendChild(new PublishersItemView({model: publisher}).render().el);
+        });
+        self.$el.append(allSubViews);
+      }
+    });
+  }
 
 });
 
 PublishersItemView = Backbone.View.extend({
 
-	tagName: 'tr',
-	template: _.template($("#publishers-list-item").html()),
-	render: function() {
+  tagName: 'tr',
+
+  template: _.template($("#publishers-list-item").html()),
+
+  render: function () {
     this.$el.html(this.template(this.model.toJSON()));
     return this;
   }
@@ -42,85 +45,90 @@ PublishersItemView = Backbone.View.extend({
 
 EditPublisher = Backbone.View.extend({
 
-	el: '.container',
-	template: _.template($('#edit-publisher').html()),
-	render: function(options){
-		var self = this;
-		if (options.id) {
-			self.publisher = new Publisher({id: options.id});
-			self.publisher.fetch({
-				success: function(publisher){
-					self.$el.html(self.template({publisher: publisher}));
-				}
-			});
-		} else {
-			this.$el.html(this.template({publisher: null}));
-		}				
-	},
-	events: {
-		'submit .edit-publisher-form': 'savePublisher',
-		'click .delete' : 'deletePublisher',
-		'click .return' : 'returnToList'
-	},
-	savePublisher: function(ev){
-		var self = this;
-		self.publisherDetails = $(ev.currentTarget).serializeObject();
-		self.publisher = new Publisher();
-		Backbone.Validation.bind(this, {
+  el: '.container',
+
+  template: _.template($('#edit-publisher').html()),
+
+  render: function ( options ) {
+    var self = this;
+    if ( options.id ) {
+      self.publisher = new Publisher({id: options.id});
+      self.publisher.fetch({
+        success: function ( publisher ) {
+          self.$el.html(self.template({publisher: publisher}));
+        }
+      });
+    } else {
+      this.$el.html(this.template({publisher: null}));
+    }
+  },
+
+  events: {
+    'submit .edit-publisher-form': 'savePublisher',
+    'click .delete' : 'deletePublisher',
+    'click .return' : 'returnToList'
+  },
+
+  savePublisher: function ( ev ) {
+    var self = this;
+    self.publisherDetails = $(ev.currentTarget).serializeObject();
+    self.publisher = new Publisher();
+    Backbone.Validation.bind(this, {
       model: self.publisher
     });
     var errors = self.publisher.preValidate(self.publisherDetails);
-    if (errors) {
-    	var $errors = $('ul#errors');
-    	var msgs = _.values(errors);
-    	$errors.empty();
-    	_.each(msgs, function (msg) {
-    		$errors.append("<li>"+ msg + "</li>");
-    	});
+    if ( errors ) {
+      var $errors = $('#errors');
+      var msgs = _.values(errors);
+      $errors.empty();
+      _.each( msgs, function ( msg ) {
+        $errors.append("<li>"+ msg + "</li>");
+      });
     } else {
-			self.publisher.save(self.publisherDetails, {
-				success: function(){
-					alert("Dane zapisano");
-					self.undelegateEvents();
-					router.navigate('/publishers', {trigger:true});
-				}
-			});
-		}
-		return false;
-	},
-	deletePublisher: function(ev){
-		var self = this;
-  	var $errors = $('ul#errors');
-  	self.books = new Books();
-  	self.books.fetch({
-    	success: function () {
-      	var msg = "There are still books connected to this publisher (please delete them first)";
-      	var bookCount = 0;
-      	_.each(self.books.models, function(book) {
-        	if (String(book.attributes.publisher_id) === String(self.publisher.id)) {             
-          	bookCount += 1;
-        	}
-      	});
-      	if (bookCount > 0) {
-        	$errors.empty();
-        	$errors.append("<li>"+ msg + "</li>");
-      	} 
-      	else {
-        	if (confirm('Are you sure you want to delete that publisher?')) {
-          	self.publisher.destroy({
-            	success: function(){
-              	router.navigate('/publishers', {trigger:true});
-            	}
-          	});
-        	}
-      	}       
-    	}
-  	});
-	},
-	returnToList: function(){
-		this.undelegateEvents();
-		router.navigate('/publishers', {trigger:true});
-		return false;
-	}
+      self.publisher.save(self.publisherDetails, {
+        success: function () {
+          alert("Dane zapisano");
+          self.undelegateEvents();
+          router.navigate('/publishers', {trigger:true});
+        }
+      });
+    }
+    return false;
+  },
+
+  deletePublisher: function ( ev ) {
+    var self = this;
+    var $errors = $('#errors');
+    self.books = new Books();
+    self.books.fetch({
+      success: function () {
+        var msg = "There are still books connected to this publisher (please delete them first)";
+        var bookCount = 0;
+        _.each( self.books.models, function ( book ) {
+          if ( String(book.attributes.publisher_id) === String(self.publisher.id) ) {
+            bookCount += 1;
+          }
+        });
+        if ( bookCount > 0 ) {
+          $errors.empty();
+          $errors.append("<li>"+ msg + "</li>");
+        } else {
+          if ( confirm('Are you sure you want to delete that publisher?') ) {
+            self.publisher.destroy({
+              success: function () {
+                router.navigate('/publishers', {trigger:true});
+              }
+            });
+          }
+        }
+      }
+    });
+  },
+
+  returnToList: function () {
+    this.undelegateEvents();
+    router.navigate('/publishers', {trigger:true});
+    return false;
+  }
 
 });
